c8a75766acb0c2717626263b0ab70afce5ed9381
Vlad <115177420+MelnikVladyslav@users.noreply.github.com>
2024-11-15 10:00:22 +0200

    Initial commit

---
+++ Assets/Scripts/Clasess/Choise/ElementChoise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Clasess.Choise
+{
+    [Serializable]
+    public class ElementChoise
+    {
+        public int Id;
+        public string Name;
+        public List<Parametrs> Result;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Choice-of-a-Nation
+ 
